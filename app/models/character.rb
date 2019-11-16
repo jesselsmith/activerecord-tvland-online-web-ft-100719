d275dd358547fb227f1ab91fb0d3cc519b3be609
Jesse <jesse.lee.smith@gmail.com>
2019-11-15 16:41:14 -0800

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show, foreign_key: "show_id"
+  belongs_to :actor, foreign_key: "actor_id"
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
